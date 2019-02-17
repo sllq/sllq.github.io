8b2a6e7fc3432c6bf7e86706d289ad72eaac0bbc
sllq <axekote21@gmail.com>
2019-02-17 16:14:40 +0500

    Add files via upload

---
+++ precache-manifest.4c532ff9960d4c0687e054dacaac7301.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "80ba308207b3287be9214d8877f77753",
+    "url": "/static/media/base.80ba3082.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "42a33d5b73e0db44ad70",
+    "url": "/static/js/main.42a33d5b.chunk.js"
+  },
+  {
+    "revision": "063ce5bc9f8a30d66648",
+    "url": "/static/js/2.063ce5bc.chunk.js"
+  },
+  {
+    "revision": "42a33d5b73e0db44ad70",
+    "url": "/static/css/main.2859f028.chunk.css"
+  },
+  {
+    "revision": "79a473ab5417f7d3e81749b944f6de9d",
+    "url": "/index.html"
+  }
+];
