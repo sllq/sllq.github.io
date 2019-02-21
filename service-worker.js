b57429604a17416eca35acae5c18a22eb9e95c78
sllq <axekote21@gmail.com>
2019-02-22 01:47:57 +0500

    jquery sliding

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.49b7449841f3fbcc5433f088f24d149b.js"
+  "/precache-manifest.7b122b889b6612e854418facc79fdfbc.js"
 );
 
 workbox.clientsClaim();
