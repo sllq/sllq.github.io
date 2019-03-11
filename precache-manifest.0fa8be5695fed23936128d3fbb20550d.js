6635f1dd28b0fccd41de7b2da847035a4f893ca5
sllq <axekote21@gmail.com>
2019-03-11 22:03:54 +0500

    css fixes

---
+++ precache-manifest.0fa8be5695fed23936128d3fbb20550d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0b93acd16b554630c203b155c9a21a1",
+    "url": "/static/media/3.b0b93acd.gif"
+  },
+  {
+    "revision": "fd3c96aee54793d928460d62fcb0079e",
+    "url": "/static/media/2.fd3c96ae.jpg"
+  },
+  {
+    "revision": "80ba308207b3287be9214d8877f77753",
+    "url": "/static/media/1.80ba3082.jpg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "dd5a9587f6144dd6ead0",
+    "url": "/static/js/main.dd5a9587.chunk.js"
+  },
+  {
+    "revision": "3362b5a2dce61cb0292b",
+    "url": "/static/js/2.3362b5a2.chunk.js"
+  },
+  {
+    "revision": "dd5a9587f6144dd6ead0",
+    "url": "/static/css/main.95e89102.chunk.css"
+  },
+  {
+    "revision": "0754e3c0958b4af144f376389fda30a5",
+    "url": "/index.html"
+  }
+];
